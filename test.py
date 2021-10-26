e3b4d48e69f6c670b60adb53b552afb30a2ee479
Yuki NAKAMURA <86539208+yknaka@users.noreply.github.com>
2021-10-26 19:38:19 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import src.consoleoptions as consoleoptions
-
-dict = consoleoptions.get_dict()
-for key, value in dict.items():
-  print("key:", key, " value:", value, " valuetype:", type(value))
-
-# check command
-# python3 test.py value1="test" value2='test2' value3=test3 flagtest value4__int='43' value5__double='33' list__int=1,2,3
