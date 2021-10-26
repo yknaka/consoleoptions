6c401c6cb5ac6cebaa0711b7641863bde3b43615
Yuki NAKAMURA <86539208+yknaka@users.noreply.github.com>
2021-10-26 17:39:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
   long_description = fh.read()
 
 setuptools.setup(
-    name="get_console_option",
+    name="consoleoptions",
     version="1.4.0",
     author="Yuki NAKAMURA",
     author_email="naka_yk@live.jp",
     description="Create Console Option Dictionary",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yknaka/get_console_options",
+    url="https://github.com/yknaka/consoleoptions",
     project_urls={
-        "Create Console Option Dictionary": "https://github.com/yknaka/get_console_options",
+        "Create Console Option Dictionary": "https://github.com/yknaka/consoleoptions",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -20,12 +20,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    py_modules=['get_console_option'],
+    py_modules=['consoleoptions'],
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
     entry_points={
         'console_scripts': [
-            'get_console_options = get_console_options:main'
         ]
     },
 )
